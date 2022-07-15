f7fd1b8f3555d9e06523443c4f37cc71749f8866
v <mondalbdyut@gmail.com>
2022-07-15 07:41:53 +0530

    third commit

--- src/Services/Actions/Action.js
+++ src/Services/Actions/Action.js
@@ -1,13 +1,13 @@
 import { ADD_TO_CART } from '../Constant'
-export const addtocart = (data) => {
+export const addToCart = (data) => {
     return {
         type: ADD_TO_CART,
         data: data
     }
 }
-export const removetocart = (data) => {
-    return {
-        type: "REMOVE_TO_CART",
-        data: data
-    }
-}
+// export const removetocart = (data) => {
+//     return {
+//         type: "REMOVE_TO_CART",
+//         data: data
+//     }
+// }
