afd08981005314b1bd46af977563a746690c7e79
v <mondalbdyut@gmail.com>
2022-07-20 20:12:46 +0530

    third commit

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,15 +1,15 @@
 import { connect } from 'react-redux'
 import Home from './../components/Home';
-import { addToCart } from './../Services/Actions/Action';
+import { addToCart, removeToCart } from './../Services/Actions/Action';
 
 const mapStateToProps = state => ({
-    cardData: state
+    data: state.cardItems
 })
 
 const mapDispatchToProps = dispatch => ({
-    addToCartHandler: data => dispatch(addToCart(data))
+    addToCartHandler: data => dispatch(addToCart(data)),
+    removeToCartHandler: data => dispatch(removeToCart(data))
 })
 
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home)
-//export default Home;
